1a8e4b4176ccd520e7dbf545a4466aa21dbcee02
MohBahrulUlum15 <belajarkoding97@gmail.com>
2023-08-07 13:00:55 +0700

    commit dicoding_news_app

---
+++ dicoding_news_app/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:dicoding_news_app/models/article.dart';
+import 'package:dicoding_news_app/views/ArticleWebView.dart';
+import 'package:dicoding_news_app/views/detail_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // if (Platform.isAndroid) {
+  //   WebView.platform = SurfaceAndroidWebView();
+  // } else if(Platform.isIOS) {
+  //   WebView.platform = CupertinoWebView();
+  // }
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: NewsListPage.routeName,
+      routes: {
+        NewsListPage.routeName: (context) => const NewsListPage(),
+        ArticleDetailPage.routeName: (context) => ArticleDetailPage(
+              article: ModalRoute.of(context)?.settings.arguments as Article,
+            ),
+        ArticleWebView.routeName: (context) => ArticleWebView(
+              url: ModalRoute.of(context)?.settings.arguments as String,
+            ),
+      },
+    );
+  }
+}
+
+class NewsListPage extends StatelessWidget {
+  static const routeName = '/article_list';
+
+  const NewsListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: FutureBuilder(
+        future:
+            DefaultAssetBundle.of(context).loadString('assets/articles.json'),
+        builder: (context, snapshot) {
+          final List<Article> articles = parseArticles(snapshot.data);
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              return _buildArticleItem(context, articles[index]);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+Widget _buildArticleItem(BuildContext context, Article article) {
+  return ListTile(
+    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    leading: Image.network(
+      article.urlToImage,
+      width: 100,
+      errorBuilder: (ctx, error, _) => const Center(
+        child: Icon(Icons.error),
+      ),
+    ),
+    title: Text(article.title),
+    subtitle: Text(article.author),
+    onTap: () {
+      Navigator.pushNamed(context, ArticleDetailPage.routeName,
+          arguments: article);
+    },
+  );
+}
